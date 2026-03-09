3069eee8deca3a18a31f483a1da19dae861cac75
Aleksandra Temereva <aleksandra.temereva@epita.fr>
2026-03-09 20:51:45 +0100

    changed the entry gate img visual

--- frontend/src/entryGate/EntryGate.jsx
+++ frontend/src/entryGate/EntryGate.jsx
@@ -27,37 +27,9 @@ function EntryGate({ onEnter }) {
         This is how love wins fear
       </p>
       <div className="entryGateHeart" aria-hidden="true">
-        <div className="entryGateHeartField" />
-        <div className="entryGateHeartGlow" />
-        <div className="entryGateHeartOuter" />
-        <div className="entryGateHeartInner" />
-        <span className="entryGateOrbit entryGateOrbitA" />
-        <span className="entryGateOrbit entryGateOrbitB" />
-        <div className="entryGateHeartParticles">
-          {Array.from({ length: 10 }).map((_, index) => (
-            <span
-              key={`entry-heart-particle-${index}`}
-              className="entryGateHeartParticle"
-              style={{
-                "--entry-particle-i": index,
-                "--entry-particle-delay": `${index * 120}ms`,
-              }}
-            />
-          ))}
-        </div>
-
-        <div className="entryGateOrbitBeads">
-          {Array.from({ length: 8 }).map((_, index) => (
-            <span
-              key={`entry-orbit-bead-${index}`}
-              className="entryGateOrbitBead"
-              style={{
-                "--entry-bead-i": index,
-                "--entry-bead-delay": `${index * 140}ms`,
-              }}
-            />
-          ))}
-        </div>
+        <span className="entryGateCoreField" />
+        <span className="entryGateCoreRing" />
+        <span className="entryGateCore" />
       </div>
       <button className="entryGateButton" type="button" onClick={onEnter}>
         begin
